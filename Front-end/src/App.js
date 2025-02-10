38ba030c40fdafbbebbf78966dead4ca469b2587
yuik <163374559+yuik0712@users.noreply.github.com>
2025-02-10 14:12:26 +0900

    [Move] 파일 분리
    # 필요없는 파일 제거하였습니다.
    # 모든 레이아웃 & 스타일 요소들을 Components 풀더로 파일 이동 시켰습니다.

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Calendar from "./Components/Main_popup/Calendar/Calendar";
-import Contents from "./Components/Main/Contents/Contents";
-import PopupPassword from "./Components/Main/Popup/Password/PopupPassword";
+import Contents from "./Components/Contents/Contents";
+import PopupPassword from "./Components/Popup/Password/PopupPassword";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
           <Route exact path="/" element={<Contents />} />
           <Route exact path="/Contents" element={<Contents />} /> {/* 추가됨 */}
           <Route exact path="/password" element={<PopupPassword />} />
-          <Route exact path="/Calendar" element={<Calendar />} />
         </Routes>
       </Router>
     </div>
